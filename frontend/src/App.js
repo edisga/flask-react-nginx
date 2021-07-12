36c42e34fc1ab4c822f7dc766bdc14eac8fedf71
Edison Garcia <egachi@live.com>
2021-07-12 15:18:14 -0500

    Adding Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,47 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import ProductList from './components/Products';
 import withListLoading from './components/ProductsLoading';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
   const ListLoading = withListLoading(ProductList);
   const [appState, setAppState] = useState({
     loading: false,
@@ -12,7 +51,7 @@ function App() {
 
   useEffect(() => {
     setAppState({ loading: true });
-    const apiUrl = `http://0.0.0.0/api/products`;
+    const apiUrl = `http://127.0.0.1/api/products`;
     fetch(apiUrl)
       .then((res) => res.json())
       .then((products) => {
@@ -27,4 +66,11 @@ function App() {
     </div>
   );
 }
-export default App;
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
